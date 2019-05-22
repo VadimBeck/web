de5750d89f6e67fde1d387e8362497ac07a6a6fc
Vadim Bekitko <MasterH231AM@yandex.ru>
2019-05-22 15:41:37 +0300

    ios debug_1

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,8 +1,7 @@
-const header = document.getElementsByClassName('header')[0];
+const header = document.querySelector('.header');
 
 document.querySelector('.menu-btn').addEventListener('click', function(e) {
   e.preventDefault();
   header.classList.toggle('is-active');
   this.classList.toggle('is-active');
 })
-
--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,28 +1,26 @@
-let slides = document.getElementsByClassName('candy-bar__slide');
-let back = document.getElementById('back');
-let forward = document.getElementById('forward');
+var d = document;
+var list = d.querySelector('.candy-bar__list');
+var items = d.getElementsByClassName('candy-bar__item');
+var itemCount = d.querySelectorAll('.candy-bar__item').length;
+var scroll = d.querySelector('.candy-bar__scroll');
+var back = d.querySelector('#back');
+var forward = d.querySelector('#forward');
+var pos = 0;
 
-let i=0;
-back.onclick = function() { 
-  if (i+1 == slides.length) {
-    slides[i].classList.remove('is-active');
-    i=0;
-  } else {
-  slides[i].classList.toggle('is-active');
-  i++;
-  }  
-  slides[i].classList.toggle('is-active');
+function setTransform() {
+  list.style.transform = 'translateX(' + (-pos * scroll.offsetWidth) + 'px)';
 }
 
-forward.onclick = function() { 
-  if (i == 0) {
-    slides[i].classList.remove('is-active');
-    i=slides.length-1;
-  } else {
-  slides[i].classList.toggle('is-active');
-  i--;
-  }  
-  slides[i].classList.toggle('is-active');
+function prev() {
+  pos = Math.max(pos - 1, 0);
+  setTransform();
+}
+function next() {
+  pos = Math.min(pos + 1, itemCount - 1);
+  setTransform();
 }
 
+back.addEventListener('click', prev);
+forward.addEventListener('click', next);
 
+window.addEventListener('resize', setTransform);
