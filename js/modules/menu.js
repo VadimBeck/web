77e3d00239fbd9a0e53408a985787ebd0977ad2d
Vadim Bekitko <MasterH231AM@yandex.ru>
2019-05-24 12:12:22 +0300

    ТЗ_3.1

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,7 +1,42 @@
-const header = document.querySelector('.header');
+const menu = (function() {
+  let button = document.querySelector('.menu-btn');
+  let header = document.querySelector('.header');
+  let body = document.querySelector('body');
+  let nav = document.querySelector('.nav__list');
+  let act = false;
 
-document.querySelector('.menu-btn').addEventListener('click', function(e) {
-  e.preventDefault();
-  header.classList.toggle('is-active');
-  this.classList.toggle('is-active');
-})
+  function toggleMenu() {
+    if (body.offsetWidth > 768) {
+      return
+    } else {
+      button.classList.toggle('is-active');
+      header.classList.toggle('is-active');
+      body.classList.toggle('locked');
+
+      act ? act = false : act = true;
+    }
+  }
+
+  let addListeners = function() {
+        button.addEventListener('click', toggleMenu);
+
+        nav.addEventListener('click', function(e){
+          let target = e.target;
+          if (target.classList.contains('nav__link')) {
+            toggleMenu();
+          }
+        });
+      }
+      document.addEventListener('keydown', function(e){
+        if (e.keyCode == 27 && act) {
+          toggleMenu();
+        }
+      })
+
+  return {
+    menuOver: addListeners  
+  }
+
+}());
+
+menu.menuOver();
--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,48 +1,51 @@
-var d = document;
-var list = d.querySelector('.candy-bar__list');
-var itemCount = d.querySelectorAll('.candy-bar__item').length;
-var slider = d.querySelector('.candy-bar__slider');
-var back = d.querySelector('#back');
-var forward = d.querySelector('#forward');
-var pos = 0;
-var clientX, clientY;
+(function() {
+    let d = document;
+    let list = d.querySelector('.candy-bar__list');
+    let itemCount = d.querySelectorAll('.candy-bar__item').length;
+    let slider = d.querySelector('.candy-bar__slider');
+    let back = d.querySelector('#back');
+    let forward = d.querySelector('#forward');
+    let pos = 0;
+    var clientX, clientY;
 
 //Слайдер
 
-function setTransform() {
-  list.style.transform = 'translateX(' + (-pos * slider.offsetWidth) + 'px)';
-}
+  function setTransform() {
+    list.style.transform = 'translateX(' + (-pos * slider.offsetWidth) + 'px)';
+  }
 
-function prev() {
-  pos = Math.max(pos - 1, 0);
-  setTransform();
-}
-function next() {
-  pos = Math.min(pos + 1, itemCount - 1);
-  setTransform();
-}
+  function prev() {
+    pos = Math.max(pos - 1, 0);
+    setTransform();
+  }
+  function next() {
+    pos = Math.min(pos + 1, itemCount - 1);
+    setTransform();
+  }
 
-back.addEventListener('click', prev);
-forward.addEventListener('click', next);
-window.addEventListener('resize', setTransform);
+  back.addEventListener('click', prev);
+  forward.addEventListener('click', next);
+  window.addEventListener('resize', setTransform);
 
 //Обработка события
 
-slider.addEventListener('touchstart', function(e) {  
-  clientX = e.touches[0].clientX;
-  clientY = e.touches[0].clientY;
-}, false);
+  slider.addEventListener('touchstart', function(e) {  
+    clientX = e.touches[0].clientX;
+    clientY = e.touches[0].clientY;
+  }, false);
 
-slider.addEventListener('touchend', function(e) {
-  var deltaX, deltaY;
-  
-  deltaX = e.changedTouches[0].clientX - clientX;
-  deltaY = e.changedTouches[0].clientY - clientY;
-  var mod = Math.abs(deltaX) - Math.abs(deltaY);
+  slider.addEventListener('touchend', function(e) {
+    var deltaX, deltaY;
+    
+    deltaX = e.changedTouches[0].clientX - clientX;
+    deltaY = e.changedTouches[0].clientY - clientY;
+    var mod = Math.abs(deltaX) - Math.abs(deltaY);
 
-  if (deltaX < 0 && mod > 0) {
-    next();
-  } else if (deltaX > 0 && mod > 0) {
-    prev();
-  }   
-}, false);
+    if (deltaX < 0 && mod > 0) {
+      next();
+    } else if (deltaX > 0 && mod > 0) {
+      prev();
+    }   
+  }, false);
+
+}());
