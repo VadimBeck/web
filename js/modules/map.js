03974b778d6bab17ec16d0850843a84cd1041915
Vadim Bekitko <MasterH231AM@yandex.ru>
2019-06-07 12:38:50 +0300

    pngCatch

--- js/modules/map.js
+++ js/modules/map.js
@@ -37,7 +37,7 @@ function init() {
       },
       {
         iconLayout: 'default#image',
-        iconImageHref: '../img/mapMarker.png',
+        iconImageHref: '../img/marker.png',
         iconImageSize: [46, 57],
         iconImageOffset: [-23, -57]
       });
@@ -45,7 +45,7 @@ function init() {
   var clusterer = new ymaps.Clusterer({
     clusterIcons: [
       {
-        href: '../img/mapMarker.png',
+        href: '../img/marker.png',
         size: [46, 57],
         offset: [-23, -57]
       }
--- js/modules/video.js
+++ js/modules/video.js
@@ -1,67 +1,69 @@
-let videoPlayer = function(){
+document.addEventListener('DOMContentLoaded', function() {
+  let videoPlayer = function(){
 
-  let video = document.querySelector('.video__content');
-  let playBtn = document.querySelector('.video__btn');
-  let volume = document.querySelector('.video__volume');
-  let progressBar = document.querySelector('.video__progress');
-  let switchBtn = document.querySelector('.video__play');
-  let playing = false;
-
-  video.ontimeupdate = function(){
-    progress();
-    if(video.currentTime == video.duration){
-      pause();
-    }
-  } 
-  function play() {
-    video.play();
-    playing = true;
-    playBtn.classList.add('hidden');
-    switchBtn.classList.add('stop');
-  }
-  function pause() {
-    video.pause();
-    playing = false;
-    playBtn.classList.remove('hidden');
-    switchBtn.classList.remove('stop');
-  }
-  function progress(){
-    let duration = video.duration;
-    let time = video.currentTime;
-    progressBar.value = (time/duration)*100;
-  }
-
-  playBtn.addEventListener('click', function(){
-    if(!playing) {
-      play();
-    }
-  });
-  video.addEventListener('click', function(){
-      if(playing) {
+    let video = document.querySelector('.video__content');
+    let playBtn = document.querySelector('.video__btn');
+    let volume = document.querySelector('.video__range--volume');
+    let progressBar = document.querySelector('.video__range--progress');
+    let switchBtn = document.querySelector('.video__play');
+    let playing = false;
+  
+    video.ontimeupdate = function(){
+      progress();
+      if(video.currentTime == video.duration){
         pause();
       }
-  });
-  volume.addEventListener('input', function(){
-    let value = this.value;
-    video.volume = value/100;
-  })
-  progressBar.addEventListener('click', function(e) {
-    let width = progressBar.offsetWidth;
-    let pos = e.offsetX;
-    progressBar.value = (pos/width)*100;
-    video.pause();
-    video.currentTime = video.duration*(pos/width);
-    video.play();
-  })
-  switchBtn.addEventListener('click',function(){
-    if(!playing) {
-      play();
+    } 
+    function play() {
+      video.play();
+      playing = true;
+      playBtn.classList.add('hidden');
+      switchBtn.classList.add('stop');
     }
-    else if(playing) {
-      pause();
-      video.currentTime = 0;
+    function pause() {
+      video.pause();
+      playing = false;
+      playBtn.classList.remove('hidden');
+      switchBtn.classList.remove('stop');
     }
-  })
-
-}
-videoPlayer();
+    function progress(){
+      let duration = video.duration;
+      let time = video.currentTime;
+      progressBar.value = (time/duration)*100;
+    }
+  
+    playBtn.addEventListener('click', function(){
+      if(!playing) {
+        play();
+      }
+    });
+    video.addEventListener('click', function(){
+        if(playing) {
+          pause();
+        }
+    });
+    volume.addEventListener('input', function(){
+      let value = this.value;
+      video.volume = value/100;
+    })
+    progressBar.addEventListener('click', function(e) {
+      let width = progressBar.offsetWidth;
+      let pos = e.offsetX;
+      progressBar.value = (pos/width)*100;
+      video.pause();
+      video.currentTime = video.duration*(pos/width);
+      video.play();
+    })
+    switchBtn.addEventListener('click',function(){
+      if(!playing) {
+        play();
+      }
+      else if(playing) {
+        pause();
+        video.currentTime = 0;
+      }
+    })
+  
+  }
+  videoPlayer();
+}, false);
